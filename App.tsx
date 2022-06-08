1d87105cf23449f4d09e286bfa36c014e9f8003e
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-08 14:47:53 -0300

    feat: added font Inter with @expo-google-fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+
+import { View, Text, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+
 import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World</Text>
